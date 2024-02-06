a42bac88a8d17c3760d333c38e5323248f845469
Vinay Kumar Chowdry <50494494+vinayvk31@users.noreply.github.com>
2024-02-06 12:15:39 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -13,10 +13,9 @@ const Footer = () => {
             <h4>Developed by <b>Vinay Kumar</b></h4>
             <h4><b>Copyright</b> &copy; {year} CV </h4>
             <div className='footerLinks'>
-                <a href="#" target='_blank' rel="noreferrer"><FaGithub/></a>
+                <a href="https://github.com/vinayvk31" target='_blank' rel="noreferrer"><FaGithub/></a>
                 <a href="https://www.linkedin.com/in/vinay-kumar-chowdry/" target='_blank' rel="noreferrer"><FaLinkedin/></a>
                 <a href='mailTo:c.vinnu6c@gmail.com' target='_blank' rel="noreferrer"><GrMail/></a>
-                <a href="#" target="_blank" rel="noreferrer"><BsTwitterX/></a>
             </div>
         </footer>
     );
